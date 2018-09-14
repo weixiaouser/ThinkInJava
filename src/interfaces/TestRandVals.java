b01cdb778303e39431a1d6eaf9378c6fd97374f1
WeiXiao <weixiao930101@163.com>
2018-09-14 16:43:58 +0800

    interfaces finished

---
+++ src/interfaces/TestRandVals.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+/**
+ * Created by weixiao on 2018/9/12.
+ */
+public class TestRandVals {
+    public static void main(String[] args) {
+        System.out.println(RandVals.RANDOM_INT);
+        System.out.println(RandVals.RANDOM_LONG);
+        System.out.println(RandVals.RANDOM_FLOAT);
+        System.out.println(RandVals.RANDOM_DOUBLE);
+    }
+}/*output:
+-1172028779
+-32032247016559954
+0.534122
+1.6020656493302599
+*/
