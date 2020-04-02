5377ee2f7b97f8426aac42ec0c0d64e9ced52aab
WeiXiao <weixiao930101@163.com>
2020-04-02 21:14:05 +0800

    单例模式

---
+++ src/juc/single/Hungry.java
@@ -0,0 +1,21 @@
+package juc.single;
+
+/**
+ * @author :weixiao
+ * @description : 饿汉式单例模式
+ * 饿汉式顾明司仪就是很饿所以要new出来
+ * 缺点就是类一加载就会占用内存空间
+ * @date :2020/4/2 20:53
+ */
+public class Hungry {
+
+    private static final Hungry hungry = new Hungry();
+    //单例模式的核心思想就是构造器私有
+    private Hungry(){
+
+    }
+
+    public Hungry getInstance(){
+        return hungry;
+    }
+}
