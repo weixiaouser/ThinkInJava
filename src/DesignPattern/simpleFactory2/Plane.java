ed56f65258e926da2f54c8fd2efafe7859d9594f
WeiXiao <weixiao930101@163.com>
2020-05-27 10:03:51 +0800

    simpleFactory 简单工厂

---
+++ src/DesignPattern/simpleFactory2/Plane.java
@@ -0,0 +1,14 @@
+package DesignPattern.simpleFactory2;
+
+/**
+ * @author :weixiao
+ * @description :
+ * @date :2020/5/26 20:50
+ */
+public class Plane implements Moveable{
+
+    @Override
+    public void go(){
+        System.out.println("Plane gogogogo...");
+    }
+}
