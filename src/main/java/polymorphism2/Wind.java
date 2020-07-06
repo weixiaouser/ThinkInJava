7ef6d00fdfeeceaffa962214e434476f55c72048
WeiXiao <weixiao930101@163.com>
2020-07-06 13:49:27 +0800

    转换为maven管理

---
+++ src/main/java/polymorphism2/Wind.java
@@ -0,0 +1,19 @@
+package polymorphism2;
+
+import polymorphism.Note;
+
+/**
+ * Created by weixiao on 2018/8/12.
+ * polymorphism/music Instrument.java
+ */
+
+class Instrument{
+    public void play(Note n){
+        System.out.println("Instrument.play()");
+    }
+}
+public class Wind extends Instrument {
+    public void play(Note n){
+        System.out.println("Wind.play()"+n);
+    }
+}
